b8fac3fca7d81b2b715b8e6be069d5e2ad10b2a2
Kristoffer Andersen <kja@cs.au.dk>
2019-02-24 21:40:36 +0100

    More raft modelling

--- src/NetSim/Examples/Raft.hs
+++ src/NetSim/Examples/Raft.hs
@@ -1,7 +1,54 @@
-{-# LNAGUAGE GADTs, KindSignatures #-}
+{-# LANGUAGE GADTs #-}
 module NetSim.Examples.Raft where
 
 import NetSim.Types
 import NetSim.Specifications
 
-data S = Actor
+data S = Actor {
+  -- Persistent State
+  _currentTerm :: Int,
+  _votedFor :: Maybe NodeID,
+  _log :: [Int],
+  -- Volatile State
+  _comittedIndex :: Int,
+  _lastApplied :: Int,
+  -- Volatile State on Leaders
+  _nextIndex :: [Int],
+  _matchIndex :: [Int]
+  }
+
+  | Client NodeID Int
+  | ClientDone
+
+data Mode = Follower
+          | Candidate
+          | Leader
+
+{-
+      [0]                     [1]                       [2]
+    ------------> Follower --------------> Candidate --------------> Leader
+                  ^  ^                       |                        |
+                  |  |         [3]           |                        |
+                  |  |_______________________|                        |
+                  |                                                   |
+                  |                                 [4]               |
+                  |___________________________________________________|
+
+    - [0] Starts up | Recovers
+    - [1] Times out | Starts election
+    - [2] Receives votes from majority of servers and becomes leader
+    - [3] Discovers leader of new term | Discovers candidate with a higher term
+    - [4] Discovers server with higher term
+-}
+
+perform :: Alternative f => Protlet f S
+perform = ARPC "perform" clientStep serverRec serverSend
+  where
+    clientStep s = case s of
+      Client server n -> Just ([n], server, const ClientDone)
+      _ -> Nothing
+    
+    serverRec msg s = case s of
+      
+
+    serverSend = undefined
